956353aaf96a47713784d6890091753a594fd1f2
Omargherita <omaralkhateeb080@gmail.com>
2026-08-18 16:48:57 +0300

    Fresh Start

--- TestingProj/Form1.cs
+++ TestingProj/Form1.cs
@@ -16,27 +16,5 @@ namespace TestingProj
         {
             InitializeComponent();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox2.Text;
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            textBox2.Enabled = false;
-            textBox1.Visible = false;
-            textBox2.BackColor = Color.Black;
-        }
     }
 }
