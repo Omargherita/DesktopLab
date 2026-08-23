00b79a2d961e37333fe5ef55c6909145c65c059d
Omargherita <omaralkhateeb080@gmail.com>
2026-08-24 02:23:01 +0300

    The Game Was Easy, I Wasn't

--- Tic-Tac-Toe Game/Form1.cs
+++ Tic-Tac-Toe Game/Form1.cs
@@ -18,9 +18,218 @@ namespace Tic_Tac_Toe_Game
             InitializeComponent();
         }
 
+        enum enPlayer { X, O }
+
+        enPlayer CurrentPlayer = enPlayer.X;
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            UpdateTurnLabel();
+            UpdateWinnerLabel();
+        }
 
+        void UpdateTurn()
+        {
+            CurrentPlayer = (CurrentPlayer == enPlayer.X ? enPlayer.O : enPlayer.X);
+        }
+
+        void UpdateTurnLabel()
+        {
+            lblTurn.Text = (CurrentPlayer == enPlayer.X ? "Player 1" : "Player 2");
+        }
+
+        void UpdateWinnerLabel()
+        {
+            lblWinner.Text = "In Progress";
+        }
+
+        void HandleClick(PictureBox pb)
+        {
+            if (pb.Tag != null)
+                return;
+
+            pb.Image = (CurrentPlayer == enPlayer.X ? Resources.X : Resources.O);
+            pb.Tag = CurrentPlayer;
+
+            UpdateGame();
+        }
+
+        bool CheckDraw()
+        {
+            PictureBox[] Board =
+            {
+                pbIndex1, pbIndex2, pbIndex3,
+                pbIndex4, pbIndex5, pbIndex6,
+                pbIndex7, pbIndex8, pbIndex9
+            };
+
+            foreach (PictureBox pb in Board)
+            {
+                if (pb.Tag == null)
+                    return false;
+            }
+
+            return true;
+        }
+
+        void UpdateGame()
+        {
+            if (CheckWinner())
+            {
+                lblTurn.Text = "Game Over";
+                lblWinner.Text = (CurrentPlayer == enPlayer.X ? "Player 1" : "Player 2");
+                DisableBoard();
+                return;
+            }
+
+            if (CheckDraw())
+            {
+                lblTurn.Text = "Game Over";
+                lblWinner.Text = "Tie";
+                DisableBoard();
+                return;
+            }
+
+            UpdateTurn();
+            UpdateTurnLabel();
+        }
+
+        void DisableBoard()
+        {
+            pbIndex1.Enabled = false;
+            pbIndex2.Enabled = false;
+            pbIndex3.Enabled = false;
+            pbIndex4.Enabled = false;
+            pbIndex5.Enabled = false;
+            pbIndex6.Enabled = false;
+            pbIndex7.Enabled = false;
+            pbIndex8.Enabled = false;
+            pbIndex9.Enabled = false;
+        }
+
+        void EnableBoard()
+        {
+            pbIndex1.Enabled = true;
+            pbIndex2.Enabled = true;
+            pbIndex3.Enabled = true;
+            pbIndex4.Enabled = true;
+            pbIndex5.Enabled = true;
+            pbIndex6.Enabled = true;
+            pbIndex7.Enabled = true;
+            pbIndex8.Enabled = true;
+            pbIndex9.Enabled = true;
+        }
+
+        private void pbIndex1_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex1);
+        }
+
+        private void pbIndex2_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex2);
+        }
+
+        private void pbIndex3_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex3);
+        }
+
+        private void pbIndex4_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex4);
+        }
+
+        private void pbIndex5_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex5);
+        }
+
+        private void pbIndex6_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex6);
+        }
+
+        private void pbIndex7_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex7);
+        }
+
+        private void pbIndex8_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex8);
+        }
+
+        private void pbIndex9_Click(object sender, EventArgs e)
+        {
+            HandleClick(pbIndex9);
+        }
+
+        void ResetPictureBox(PictureBox pb)
+        {
+            pb.Image = Resources.question_mark_96;
+            pb.Tag = null;
+            pb.BackColor = Color.Black;
+        }
+
+        void RestartGame()
+        {
+            CurrentPlayer = enPlayer.X;
+
+            EnableBoard();
+
+            UpdateTurnLabel();
+            UpdateWinnerLabel();
+
+            ResetPictureBox(pbIndex1);
+            ResetPictureBox(pbIndex2);
+            ResetPictureBox(pbIndex3);
+            ResetPictureBox(pbIndex4);
+            ResetPictureBox(pbIndex5);
+            ResetPictureBox(pbIndex6);
+            ResetPictureBox(pbIndex7);
+            ResetPictureBox(pbIndex8);
+            ResetPictureBox(pbIndex9);
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        bool CheckWin(PictureBox pb1, PictureBox pb2, PictureBox pb3)
+        {
+            if (pb1.Tag == null ||
+                pb2.Tag == null ||
+                pb3.Tag == null)
+                return false;
+
+            if ((enPlayer)pb1.Tag == (enPlayer)pb2.Tag &&
+                (enPlayer)pb2.Tag == (enPlayer)pb3.Tag)
+            {
+                pb1.BackColor = Color.Green;
+                pb2.BackColor = Color.Green;
+                pb3.BackColor = Color.Green;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        bool CheckWinner()
+        {
+            return
+                (
+                    CheckWin(pbIndex1, pbIndex2, pbIndex3) ||
+                    CheckWin(pbIndex4, pbIndex5, pbIndex6) ||
+                    CheckWin(pbIndex7, pbIndex8, pbIndex9) ||
+                    CheckWin(pbIndex1, pbIndex4, pbIndex7) ||
+                    CheckWin(pbIndex2, pbIndex5, pbIndex8) ||
+                    CheckWin(pbIndex3, pbIndex6, pbIndex9) ||
+                    CheckWin(pbIndex1, pbIndex5, pbIndex9) ||
+                    CheckWin(pbIndex3, pbIndex5, pbIndex7)
+                );
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
@@ -34,56 +243,5 @@ namespace Tic_Tac_Toe_Game
             e.Graphics.DrawLine(Pen, 425, 130, 425, 520);
             e.Graphics.DrawLine(Pen, 593, 130, 593, 520);
         }
-
-        private void pbIndex1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pbIndex9_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnPlayAgain_Click(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
