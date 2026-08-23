7d695aa436729d540abb68a9a0f873537a5acfd4
Omargherita <omaralkhateeb080@gmail.com>
2026-08-23 18:36:39 +0300

    Pixels Are Finally In Place

--- Tic-Tac-Toe Game/Form1.Designer.cs
+++ Tic-Tac-Toe Game/Form1.Designer.cs
@@ -28,13 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblWinner = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pbIndex1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbIndex4 = new System.Windows.Forms.PictureBox();
+            this.pbIndex7 = new System.Windows.Forms.PictureBox();
+            this.pbIndex8 = new System.Windows.Forms.PictureBox();
+            this.pbIndex9 = new System.Windows.Forms.PictureBox();
+            this.pbIndex5 = new System.Windows.Forms.PictureBox();
+            this.pbIndex6 = new System.Windows.Forms.PictureBox();
+            this.pbIndex3 = new System.Windows.Forms.PictureBox();
+            this.pbIndex2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(415, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 90);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic - Tac - Toe ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(85, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 57);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Turn";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTurn.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurn.ForeColor = System.Drawing.Color.White;
+            this.lblTurn.Location = new System.Drawing.Point(60, 294);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(0, 57);
+            this.lblTurn.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(60, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 57);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Winner";
+            // 
+            // lblWinner
+            // 
+            this.lblWinner.AutoSize = true;
+            this.lblWinner.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblWinner.Font = new System.Drawing.Font("Times New Roman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWinner.ForeColor = System.Drawing.Color.White;
+            this.lblWinner.Location = new System.Drawing.Point(61, 453);
+            this.lblWinner.Name = "lblWinner";
+            this.lblWinner.Size = new System.Drawing.Size(0, 57);
+            this.lblWinner.TabIndex = 4;
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayAgain.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPlayAgain.Location = new System.Drawing.Point(48, 539);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(272, 75);
+            this.btnPlayAgain.TabIndex = 5;
+            this.btnPlayAgain.Text = "Play Again";
+            this.btnPlayAgain.UseVisualStyleBackColor = false;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pbIndex1
+            // 
+            this.pbIndex1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex1.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex1.Location = new System.Drawing.Point(404, 161);
+            this.pbIndex1.Name = "pbIndex1";
+            this.pbIndex1.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex1.TabIndex = 7;
+            this.pbIndex1.TabStop = false;
+            this.pbIndex1.Click += new System.EventHandler(this.pbIndex1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tic_Tac_Toe_Game.Properties.Resources.Hollow;
+            this.pictureBox1.Location = new System.Drawing.Point(67, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbIndex4
+            // 
+            this.pbIndex4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex4.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex4.Location = new System.Drawing.Point(404, 331);
+            this.pbIndex4.Name = "pbIndex4";
+            this.pbIndex4.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex4.TabIndex = 9;
+            this.pbIndex4.TabStop = false;
+            this.pbIndex4.Click += new System.EventHandler(this.pbIndex4_Click);
+            // 
+            // pbIndex7
+            // 
+            this.pbIndex7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex7.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex7.Location = new System.Drawing.Point(404, 499);
+            this.pbIndex7.Name = "pbIndex7";
+            this.pbIndex7.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex7.TabIndex = 10;
+            this.pbIndex7.TabStop = false;
+            this.pbIndex7.Click += new System.EventHandler(this.pbIndex7_Click);
+            // 
+            // pbIndex8
+            // 
+            this.pbIndex8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex8.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex8.Location = new System.Drawing.Point(608, 499);
+            this.pbIndex8.Name = "pbIndex8";
+            this.pbIndex8.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex8.TabIndex = 11;
+            this.pbIndex8.TabStop = false;
+            this.pbIndex8.Click += new System.EventHandler(this.pbIndex8_Click);
+            // 
+            // pbIndex9
+            // 
+            this.pbIndex9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex9.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex9.Location = new System.Drawing.Point(814, 499);
+            this.pbIndex9.Name = "pbIndex9";
+            this.pbIndex9.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex9.TabIndex = 12;
+            this.pbIndex9.TabStop = false;
+            this.pbIndex9.Click += new System.EventHandler(this.pbIndex9_Click);
+            // 
+            // pbIndex5
+            // 
+            this.pbIndex5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex5.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex5.Location = new System.Drawing.Point(608, 331);
+            this.pbIndex5.Name = "pbIndex5";
+            this.pbIndex5.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex5.TabIndex = 13;
+            this.pbIndex5.TabStop = false;
+            this.pbIndex5.Click += new System.EventHandler(this.pbIndex5_Click);
+            // 
+            // pbIndex6
+            // 
+            this.pbIndex6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex6.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex6.Location = new System.Drawing.Point(814, 331);
+            this.pbIndex6.Name = "pbIndex6";
+            this.pbIndex6.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex6.TabIndex = 14;
+            this.pbIndex6.TabStop = false;
+            this.pbIndex6.Click += new System.EventHandler(this.pbIndex6_Click);
+            // 
+            // pbIndex3
+            // 
+            this.pbIndex3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex3.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex3.Location = new System.Drawing.Point(814, 161);
+            this.pbIndex3.Name = "pbIndex3";
+            this.pbIndex3.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex3.TabIndex = 15;
+            this.pbIndex3.TabStop = false;
+            this.pbIndex3.Click += new System.EventHandler(this.pbIndex3_Click);
+            // 
+            // pbIndex2
+            // 
+            this.pbIndex2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pbIndex2.Image = global::Tic_Tac_Toe_Game.Properties.Resources.question_mark_96;
+            this.pbIndex2.Location = new System.Drawing.Point(608, 161);
+            this.pbIndex2.Name = "pbIndex2";
+            this.pbIndex2.Size = new System.Drawing.Size(147, 133);
+            this.pbIndex2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIndex2.TabIndex = 16;
+            this.pbIndex2.TabStop = false;
+            this.pbIndex2.Click += new System.EventHandler(this.pbIndex2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1047, 693);
+            this.Controls.Add(this.pbIndex2);
+            this.Controls.Add(this.pbIndex3);
+            this.Controls.Add(this.pbIndex6);
+            this.Controls.Add(this.pbIndex5);
+            this.Controls.Add(this.pbIndex9);
+            this.Controls.Add(this.pbIndex8);
+            this.Controls.Add(this.pbIndex7);
+            this.Controls.Add(this.pbIndex4);
+            this.Controls.Add(this.pbIndex1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.lblWinner);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIndex2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblTurn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblWinner;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pbIndex1;
+        private System.Windows.Forms.PictureBox pbIndex4;
+        private System.Windows.Forms.PictureBox pbIndex7;
+        private System.Windows.Forms.PictureBox pbIndex8;
+        private System.Windows.Forms.PictureBox pbIndex9;
+        private System.Windows.Forms.PictureBox pbIndex5;
+        private System.Windows.Forms.PictureBox pbIndex6;
+        private System.Windows.Forms.PictureBox pbIndex3;
+        private System.Windows.Forms.PictureBox pbIndex2;
     }
 }
 
--- Tic-Tac-Toe Game/Form1.cs
+++ Tic-Tac-Toe Game/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tic_Tac_Toe_Game.Properties;
 
 namespace Tic_Tac_Toe_Game
 {
@@ -16,5 +17,73 @@ namespace Tic_Tac_Toe_Game
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color White = Color.White;
+            Pen Pen = new Pen(White);
+            Pen.Width = 10;
+
+            e.Graphics.DrawLine(Pen, 290, 250, 730, 250);
+            e.Graphics.DrawLine(Pen, 290, 390, 730, 390);
+            e.Graphics.DrawLine(Pen, 425, 130, 425, 520);
+            e.Graphics.DrawLine(Pen, 593, 130, 593, 520);
+        }
+
+        private void pbIndex1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbIndex9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
